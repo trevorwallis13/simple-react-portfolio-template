b89039cc992576ca16944e102a7db82942d1bd65
trevorwallis13 <trevor@walliscreative.com>
2022-02-17 09:17:35 -0600

    updated bio

--- src/components/about/AboutSection.js
+++ src/components/about/AboutSection.js
@@ -8,10 +8,10 @@ const AboutSection = () => {
         <h2 className="section-header">About me</h2>
         <div className="inner">
           <div className="about-text">
-            <p>Google Sheets was my gateway drug into tech. Before I’d even heard of JavaScript, I was building tools, calculators, gizmos, and gadgets (like this <a href="https://docs.google.com/spreadsheets/d/122xK6FUc5GK7mkftNWDbNx6NNJm9hR72ZPtjwVN372M/edit?usp=sharing">dice roller for DnD!</a>) in spreadsheets.</p>
-            <p>So I fell down the next flight of steps into Google App Scripts, getting my first taste of JS. I became the “Spreadsheet Guy” at my company — not because of what I could do with spreadsheets, but because of what I could make spreadsheets do for everyone else. All it took was a little bit of code and a whole lot of imagination.</p>
-            <p>But I quickly realized that you can only do so much in a spreadsheet — even with Google App Script backing you up. How much more could I do if I freed my JS knowledge from spreadsheet prison and into the real world?</p>
-            <p>That epiphany led me to make a major career change — from content writer/spreadsheet guy to software engineer.</p>
+            <p>Google Sheets was my gateway drug into tech. Before I’d even heard of JavaScript, I was building tools, calculators, gizmos, and gadgets in spreadsheets. But Sheets' native functions can only do so much, so I learned App Script as well -- giving my first taste of JavaScript.</p>
+            <p>I became the “Spreadsheet Guy” at my company — not because of what I could do with spreadsheets, but because of what I could make spreadsheets do for everyone else. All it took was a little bit of code and a whole lot of imagination.</p>
+            <p>But that led to an epiphany: How much more could I do if I freed my JS knowledge from spreadsheet prison and into the real world?</p>
+            <p>This realization completed my transformation from content writer/spreadsheet guy to software engineer. Today, I build helpful tools with JavaScript, HTML, CSS, Sass, React, Node, Express, and PostgreSQL.</p>
           </div>
           <div className="img-container">
             <div className="shadow-outline">
